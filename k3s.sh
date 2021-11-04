820a6d8ac2904197738384d23058f981bb2a13f1
romran <flowestinfo@gmail.com>
2021-11-04 21:10:17 +0200

    frontend gitflow

--- k3s.sh
+++ k3s.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+chmod 755 /tmp/
+chmod +x /tmp/vagrant-shell
+    
 echo "**** Begin installing k3s"
 curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.19.5+k3s1 K3S_KUBECONFIG_MODE="644" sh -
 echo "**** End installing k3s"

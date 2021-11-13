c8383738f7890afa9b18edd79a2813d8e881fa4d
romran <flowestinfo@gmail.com>
2021-11-13 21:23:29 +0200

    app.py update

--- reference-app/backend/app.py
+++ reference-app/backend/app.py
@@ -8,36 +8,26 @@ from jaeger_client import Config
 from prometheus_flask_exporter import PrometheusMetrics
 from flask_opentracing import FlaskTracing
 
-def init_tracer(service):
-    logging.getLogger('').handlers = []
-    logging.basicConfig(format='%(message)s', level=logging.DEBUG)
-
-    config = Config(
-        config={
-            'sampler': {
-                'type': 'const',
-                'param': 1,
-            },
-            'logging': True,
-        },
-        service_name=service,
-    )
-
-    # this call also sets opentracing.tracer
-    return config.initialize_tracer()
-
 app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'example-mongodb'
 app.config['MONGO_URI'] = 'mongodb://example-mongodb-svc.default.svc.cluster.local:27017/example-mongodb'
 mongo = PyMongo(app)
 
-# metrics = PrometheusMetrics(app, group_by='endpoint')
-metrics = PrometheusMetrics(app)
+metrics = PrometheusMetrics(app, group_by='endpoint')
 metrics.info("app_info", "App Info, this can be anything you want", version="1.0.3")
 
-backend_trace = init_tracer('backend-service')
-tracing = FlaskTracing(backend_trace, True, app)
+config = Config(
+    config={
+        'sampler':
+        {'type': 'const',
+         'param': 1},
+                        'logging': True,
+                        'reporter_batch_size': 1,}, 
+                        service_name="service")
+
+jaeger_tracer = config.initialize_tracer()
+tracing = FlaskTracing(jaeger_tracer, True, app)
 
 @app.route('/')
 def homepage():
--- reference-app/frontend/app.py
+++ reference-app/frontend/app.py
@@ -5,31 +5,41 @@ from jaeger_client import Config
 from prometheus_flask_exporter import PrometheusMetrics
 from flask_opentracing import FlaskTracing
 
-def init_tracer(service):
-    logging.getLogger('').handlers = []
-    logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+# def init_tracer(service):
+#     logging.getLogger('').handlers = []
+#     logging.basicConfig(format='%(message)s', level=logging.DEBUG)
 
-    config = Config(
-        config={
-            'sampler': {
-                'type': 'const',
-                'param': 1,
-            },
-            'logging': True,
-        },
-        service_name=service,
-    )
+#     config = Config(
+#         config={
+#             'sampler': {
+#                 'type': 'const',
+#                 'param': 1,
+#             },
+#             'logging': True,
+#         },
+#         service_name=service,
+#     )
 
-    # this call also sets opentracing.tracer
-    return config.initialize_tracer()
+#     # this call also sets opentracing.tracer
+#     return config.initialize_tracer()
 
 app = Flask(__name__)
 
 # metrics = PrometheusMetrics(app, group_by='endpoint')
-metrics = PrometheusMetrics(app)
+# metrics = PrometheusMetrics(app)
+metrics = PrometheusMetrics(app, group_by='endpoint')
 metrics.info("app_info", "App Info, this can be anything you want", version="1.0.3")
-
-tracing = FlaskTracing(init_tracer('frontend-service'), True, app)
+# config = Config(
+#     config={
+#         'sampler':
+#         {'type': 'const',
+#          'param': 1},
+#                         'logging': True,
+#                         'reporter_batch_size': 1,}, 
+#                         service_name="service")
+# jaeger_tracer = config.initialize_tracer()
+# tracing = FlaskTracing(jaeger_tracer, True, app)
+# tracing = FlaskTracing(init_tracer('frontend-service'), True, app)
 
 @app.route('/')
 def homepage():
